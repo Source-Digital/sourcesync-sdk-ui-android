d0f6393cfa5c3020978baf726054683b77afdb1c
ajaysourcedigital <ajay@sourcedigital.net>
2025-02-10 09:32:22 +0530

    init cicd

--- sourcesync-sdk-ui/build.gradle.kts
+++ sourcesync-sdk-ui/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -37,13 +38,61 @@ dependencies {
 publishing {
     publications {
         register<MavenPublication>("release") {
-            groupId = "com.github.Source-Digital"
+            groupId = "io.sourcesync.ui"
             artifactId = "sourcesync-sdk-ui-android"
-            version = "1.0.0"
+            version = System.getenv("VERSION") ?: "1.0.0-SNAPSHOT"
 
             afterEvaluate {
                 from(components["release"])
             }
+
+            pom {
+                name.set("SourceSync SDK UI Android")
+                description.set("Android UI components for SourceSync SDK")
+                url.set("https://github.com/Source-Digital/sourcesync-sdk-ui-android")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("sourcedigital")
+                        name.set("Source Digital")
+                        email.set("dev@sourcesync.io")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Source-Digital/sourcesync-sdk-ui-android.git")
+                    developerConnection.set("scm:git:ssh://github.com/Source-Digital/sourcesync-sdk-ui-android.git")
+                    url.set("https://github.com/Source-Digital/sourcesync-sdk-ui-android")
+                }
+            }
         }
     }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    val signingKeyId = System.getenv("SIGNING_KEY_ID")
+    val signingPassword = System.getenv("SIGNING_PASSWORD")
+    val signingKey = System.getenv("GPG_KEY_CONTENTS")
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    sign(publishing.publications["release"])
 }
