2484f5fca462f17f9c522c6d5b54ab2cb02fdb3f
ajaysourcedigital <ajay@sourcedigital.net>
2025-02-10 09:43:04 +0530

    Removed the manual publishing

--- sourcesync-sdk-ui/build.gradle.kts
+++ sourcesync-sdk-ui/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
-    id("maven-publish")
-    id("signing")
+    alias(libs.plugins.vanniktech.mavenPublish)
 }
 
 android {
@@ -35,64 +34,38 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "io.sourcesync.ui"
-            artifactId = "sourcesync-sdk-ui-android"
-            version = System.getenv("VERSION") ?: "1.0.0-SNAPSHOT"
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    
+    signAllPublications()
+    
+    coordinates("io.sourcesync.ui", "sourcesync-sdk-ui-android", System.getenv("VERSION") ?: "1.0.0-SNAPSHOT")
 
-            afterEvaluate {
-                from(components["release"])
-            }
-
-            pom {
-                name.set("SourceSync SDK UI Android")
-                description.set("Android UI components for SourceSync SDK")
-                url.set("https://github.com/Source-Digital/sourcesync-sdk-ui-android")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("sourcedigital")
-                        name.set("Source Digital")
-                        email.set("dev@sourcesync.io")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/Source-Digital/sourcesync-sdk-ui-android.git")
-                    developerConnection.set("scm:git:ssh://github.com/Source-Digital/sourcesync-sdk-ui-android.git")
-                    url.set("https://github.com/Source-Digital/sourcesync-sdk-ui-android")
-                }
+    pom {
+        name.set("SourceSync SDK UI Android")
+        description.set("Android UI components for SourceSync SDK")
+        inceptionYear.set("2025")
+        url.set("https://github.com/Source-Digital/sourcesync-sdk-ui-android")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("repo")
             }
         }
-    }
-
-    repositories {
-        maven {
-            name = "OSSRH"
-            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
+        developers {
+            developer {
+                id.set("sourcedigital")
+                name.set("Source Digital")
+                email.set("dev@sourcesync.io")
+                organization.set("Source Digital")
+                organizationUrl.set("https://sourcedigital.com/")
             }
         }
+        scm {
+            url.set("https://github.com/Source-Digital/sourcesync-sdk-ui-android")
+            connection.set("scm:git:git://github.com/Source-Digital/sourcesync-sdk-ui-android.git")
+            developerConnection.set("scm:git:ssh://github.com/Source-Digital/sourcesync-sdk-ui-android.git")
+        }
     }
-}
-
-signing {
-    val signingKeyId = System.getenv("SIGNING_KEY_ID")
-    val signingPassword = System.getenv("SIGNING_PASSWORD")
-    val signingKey = System.getenv("GPG_KEY_CONTENTS")
-    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-    sign(publishing.publications["release"])
 }
